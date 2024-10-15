88babad62215d62306791af6d7a5d95979967273
Maaz Ahmed <ahmedmaaz73@gmail.com>
2024-10-15 20:45:00 +0530

    Fixed copyright year and added respective JS files (#146)
    * Updating copyright year and adding JS files
    
    * Fixed placing of script tags

---
+++ scripts/about.js
@@ -0,0 +1,43 @@
+const showPopupButtons = document.querySelectorAll(".showPopupButton");
+const closePopupButton = document.querySelector(".closePopupButton");
+const popup = document.querySelector(".popup");
+const overlay = document.getElementById("overlay");
+// showPopupButton.addEventListener("click", function() {
+//   popup.style.display = "block";
+//   overlay.style.display = "block";
+// });
+showPopupButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+    // Determine which button was clicked and show the popup accordingly
+    const trigger = button.getAttribute("data-popup-trigger");
+    if (
+        trigger === "donate" ||
+        trigger === "cover-page" ||
+        trigger === "navbar"
+    ) {
+        popup.style.display = "block";
+        overlay.style.display = "block";
+    }
+    });
+});
+
+closePopupButton.addEventListener("click", function () {
+    popup.style.display = "none";
+    overlay.style.display = "none";
+});
+
+const close = document.querySelectorAll(".navclose");
+document.getElementById("openMenu").addEventListener("click", function () {
+    document.getElementById("menuContainer").style.display = "flex";
+});
+
+document.getElementById("closeMenu").addEventListener("click", function () {
+    document.getElementById("menuContainer").style.display = "none";
+});
+document.querySelectorAll(".close").addEventListener("click", function () {
+    document.getElementById("menuContainer").style.display = "none";
+});
+
+function handleClose() {
+    document.getElementById("menuContainer").style.display = "none";
+}
