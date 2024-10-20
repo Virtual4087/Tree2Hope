68e60c024ee19efc00de0e5e56b02e8c5a69a5da
Anurag Narula <anurag519narula@gmail.com>
2024-10-20 17:19:09 +0530

    Adding progress bar to index and about page (#169)

--- scripts/index.js
+++ scripts/index.js
@@ -91,3 +91,11 @@
 
   window.addEventListener('scroll', handleScroll);
 
+  window.onscroll = function() {myFunction()};
+
+  function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+  }
